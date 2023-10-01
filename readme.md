69d87d6c6536c1b1f1f1c5ded1f7d2c7c5530429
juanjo medina <juantltc99@gmail.com>
2023-10-01 16:30:56 -0300

    segunda entrega

--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
 # Simulador interactivo de gestión de alumnos y notas 📚
 
-La simulación comienza cuando el usuario ingresa que desea simular el sistema de gestión de alumnos y notas.
-Luego el usuario creará un nuevo usuario, una vez creado el sistema lo llevará al login donde deberá ingresar su apellido y contraseña. Una vez logueado podrá cargar nuevos alumnos y notas.
+La simulación comienza cuando el usuario ingresa que desea simular el sistema de compras de funkos.
 
 ### Instructivo:
 
-- Seleccionar SI para seguir la simulación
-- Crear un nuevo usuario ingresando el nombre, el apellido y la contraseña (la contraseña debe ser de al menos 4 caracteres)
-- Loguearse en el sistema con el usuario anteriormente creado
-- Cargar un nuevo alumno
+- Seleccionar SI para seguir con la simulación
+- Seleccionar un funko de la tienda (1, 2, ... , 13)
+- Confirmar que desea comprar el funko elegido
+- Seleccionar la cantidad a comprar
+- Elegir si seguir o no con la compra
--- scripts/script.js
+++ scripts/script.js
@@ -1,110 +1,270 @@
-// sistema de gestión de alumnos y notas
+const productos = [
+  {
+    nombre: "STORMTROOPER LIGTHSABER",
+    id: "funko01",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "star-wars",
+      nombre: "star wars",
+    },
+  },
+  {
+    nombre: "PIDGEOTTO",
+    id: "funko02",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "pokemon",
+      nombre: "pokemon",
+    },
+  },
+  {
+    nombre: "LUNA LOVE LIONMASK",
+    id: "funko03",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "harry-potter",
+      nombre: "harry potter",
+    },
+  },
+  {
+    nombre: "HERMIONE GRANGER",
+    id: "funko04",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "harry-potter",
+      nombre: "harry potter",
+    },
+  },
+  {
+    nombre: "HARRY POTTER",
+    id: "funko05",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "harry-potter",
+      nombre: "harry potter",
+    },
+  },
+  {
+    nombre: "PATRONUS SEVERUS SNAPE",
+    id: "funko06",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "harry-potter",
+      nombre: "harry potter",
+    },
+  },
+  {
+    nombre: "CHARMANDER",
+    id: "funko07",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "pokemon",
+      nombre: "pokemon",
+    },
+  },
+  {
+    nombre: "DRAGONITE",
+    id: "funko08",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "pokemon",
+      nombre: "pokemon",
+    },
+  },
+  {
+    nombre: "PIKACHU",
+    id: "funko09",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "pokemon",
+      nombre: "pokemon",
+    },
+  },
+  {
+    nombre: "VULPIX",
+    id: "funko10",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "pokemon",
+      nombre: "pokemon",
+    },
+  },
+  {
+    nombre: "BABY YODA",
+    id: "funko11",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "star-wars",
+      nombre: "star wars",
+    },
+  },
+  {
+    nombre: "BOBA FETH",
+    id: "funko12",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "star-wars",
+      nombre: "star wars",
+    },
+  },
+  {
+    nombre: "LUKE SKYWALKER",
+    id: "funko13",
+    stock: 10,
+    precio: 1799.99,
+    categoria: {
+      id: "star-wars",
+      nombre: "star wars",
+    },
+  },
+];
 
-let nombreProf, apellidoProf, contrasenia;
-let nota1 = 0,
-  nota2 = 0,
-  nota3 = 0;
+/* ------------------------------- */
 let bandera = true;
-
-const validarNombre = (nombre) => {
-  if (nombre === "") {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const validarContrasenia = (contrasenia) => {
-  if (contrasenia.length < 4) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const creacionDeUsuario = () => {
-  nombreProf = prompt("Ingrese su nombre");
-  while (validarNombre(nombreProf)) {
-    nombreProf = prompt(
-      "El nombre ingresado no es valido. Ingrese su nombre correctamente"
-    );
-  }
-  apellidoProf = prompt("Ingrese su apellido");
-  while (validarNombre(apellidoProf)) {
-    apellidoProf = prompt(
-      "El apellido ingresado no es valido. Ingrese su nombre correctamente"
-    );
-  }
-  contrasenia = prompt("Ingrese una contraseña de al menos 4 caracteres");
-  while (validarContrasenia(contrasenia)) {
-    contrasenia = prompt(
-      "La contraseña no cumple con la cantidad de caracteres requeridos. Ingrese una contraseña con al menos 4 caracteres"
-    );
-  }
-  alert("Usuario creado correctamente");
-};
-
-const calcularPromedio = (parcial1, parcial2, parcial3) => {
-  let promedio = (parcial1 + parcial2 + parcial3) / 3;
-  return promedio.toFixed(1);
-};
-
-const cargarAlumno = () => {
-  let nombreAlu, apellidoAlu;
-  let respuesta;
-  respuesta = prompt("Desea cargar un alumno? (SI/NO)");
-  while (respuesta.trim().toUpperCase() == "SI") {
-    nombreAlu = prompt("Ingrese el nombre del alumno: ");
-    apellidoAlu = prompt("Ingrese el apellido del alumno: ");
-    nota1 = parseInt(prompt("Ingrese la nota del primer examen: "));
-    nota2 = parseInt(prompt("Ingrese la nota del segundo examen: "));
-    nota3 = parseInt(prompt("Ingrese la nota del tercer examen: "));
-
-    alert(
-      "El alumno: " +
-        apellidoAlu +
-        ", " +
-        nombreAlu +
-        " -- NOTAS: TP1: " +
-        nota1 +
-        " - TP2: " +
-        nota2 +
-        " - TP3: " +
-        nota3 +
-        ", con promedio de: " +
-        calcularPromedio(nota1, nota2, nota3) +
-        " fue cargado correctamente."
-    );
-    respuesta = prompt("Desea cargar otro alumno? (SI/NO)");
-  }
-};
-
-const login = () => {
-  let contra, apellido;
-  alert("login");
-  while (bandera) {
-    apellido = prompt("Ingrese su apellido: ");
-    contra = prompt("ingrese su contraseña:");
-    if (
-      apellido.trim().toUpperCase() == apellidoProf.trim().toUpperCase() &&
-      contra.trim() == contrasenia.trim()
-    ) {
-      alert("Bienvenido profesor " + nombreProf);
-      cargarAlumno();
-      bandera = false;
-    } else {
-      alert("Apellido o contraseña incorrecta, vuelva a intentarlo");
-    }
-  }
-};
-
-const main = () => {
-  let respuesta;
-  respuesta = prompt("Desea simular la gestión de alumnos? (si/no)");
-  if (respuesta.trim().toUpperCase() == "SI") {
-    creacionDeUsuario();
-    login();
-  }
-};
+let respuesta, compra;
+let productosEnCarrito = [];
 
 main();
+
+function validarOpcionSiNO(op) {
+  if (op.toUpperCase().trim() == "SI") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function main() {
+  respuesta = prompt("Desea simular la compra de funkos? (SI/NO)");
+
+  validarOpcionSiNO(respuesta)
+    ? menuProductos()
+    : alert("saliendo de la simulacion");
+}
+
+function menuProductos() {
+  let nombresProductos = "";
+  productos.forEach(function concatenar(producto, i) {
+    let contador = i + 1;
+    nombresProductos =
+      nombresProductos + contador + "- " + producto.nombre + "\n";
+  });
+  opcion = parseInt(
+    prompt(
+      "TIENDA FUNKOS (Para salir presione 0)\nSeleccione un producto:\n" +
+        nombresProductos
+    )
+  );
+  validarOpcion(opcion);
+}
+
+function validarOpcion(opcion) {
+  let banderaOpcion = true;
+  let op2;
+  while (banderaOpcion) {
+    if (opcion > 0 && opcion < 14) {
+      mostrarProducto(opcion);
+      banderaOpcion = false;
+    } else if (opcion === 0) {
+      alert("terminando la simulación ");
+      banderaOpcion = false;
+    } else {
+      alert("Ingrese una opcion correcta: (respuesta esperada: 1,2,...,13)");
+      menuProductos();
+    }
+  }
+}
+function mostrarProducto(opcion) {
+  op2 = prompt(
+    "Desea comprar el funko: " +
+      productos[opcion - 1].nombre +
+      "\nPrecio: $" +
+      productos[opcion - 1].precio +
+      "\nStock: " +
+      productos[opcion - 1].stock +
+      "\n\nRESPUESTA ESPERADA: SI / NO"
+  );
+  if (op2.toLocaleLowerCase().trim() == "si") {
+    agregarAlCarrito(productos[opcion - 1], opcion - 1);
+  } else {
+    alert("volviendo al menu de productos");
+    menuProductos();
+  }
+}
+
+function agregarAlCarrito(producto, i) {
+  let cantidad, op;
+  const productoAgregado = productos.find((p) => p.id === producto.id);
+  cantidad = parseFloat(
+    prompt(
+      "Ingrese la cantidad funkos de " +
+        producto.nombre +
+        " desea comprar, stock maximo de " +
+        producto.stock
+    )
+  );
+  if (cantidad > 0 && cantidad <= productoAgregado.stock) {
+    productos[i].stock -= cantidad;
+    if (productosEnCarrito.some((p) => p.id === producto.id)) {
+      const index = productosEnCarrito.findIndex((p) => p.id === producto.id);
+      productosEnCarrito[index].cantidad += cantidad;
+    } else {
+      productoAgregado.cantidad = cantidad;
+      productosEnCarrito.push(productoAgregado);
+    }
+
+    console.log(productoAgregado);
+    op = prompt(
+      "El producto fue agregado al carrito, desea seguir comprando? (SI/NO)"
+    );
+    if (validarOpcionSiNO(op)) {
+      menuProductos();
+    } else {
+      alert(
+        "Carrito de compra: \n" +
+          concatenarCarrito() +
+          "\nEl total a pagar es: $" +
+          calcularTotal().toFixed(2)
+      );
+    }
+  } else {
+    alert("la cantidad ingresada supera el stock maximo de " + producto.stock);
+    menuProductos();
+  }
+}
+
+function calcularTotal() {
+  let total = 0;
+  productosEnCarrito.forEach(function sumar(producto) {
+    total += producto.cantidad * producto.precio;
+  });
+  return total;
+}
+
+function concatenarCarrito() {
+  let concatenacion = "";
+  productosEnCarrito.forEach(function concat(producto) {
+    concatenacion =
+      "Producto: " +
+      producto.nombre +
+      " - cantidad: " +
+      producto.cantidad +
+      " - subtotal: $" +
+      producto.cantidad * producto.precio +
+      "\n" +
+      concatenacion;
+  });
+  return concatenacion;
+}
